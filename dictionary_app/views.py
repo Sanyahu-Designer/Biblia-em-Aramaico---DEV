1dc669592b838e9d6b331fc739fb014bfbaac315
Sanyahu-Designer <sandroaugustovascao@gmail.com>
2024-12-20 21:58:21 -0300

    Atualizado em 20/12/2024 as 21h58

--- dictionary_app/views.py
+++ dictionary_app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import AramaicWord
+from django.core.paginator import Paginator
 
 def home(request):
-    words = AramaicWord.objects.all().order_by('portuguese_translation')
+    words_list = AramaicWord.objects.all().order_by('portuguese_translation')
+    paginator = Paginator(words_list, 20)  # Show 20 words per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'words': words
+        'words': page_obj,
+        'page_obj': page_obj,
     }
     return render(request, 'dictionary_app/home.html', context)
